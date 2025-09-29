b33393632963d40cfcc848b4b1b99d0f805077d0
Pedro-code-source <pedropereiraa.work@gmail.com>
2025-09-29 10:26:06 -0300

    first commit

--- back-end/cardapio/src/main/java/com/example/cardapio/food/FoodRequestDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.cardapio.food;
-
-public record FoodRequestDTO(String title, String img, Integer price) {
-    public FoodRequestDTO(Food food){
-        this(food.getTitle(), food.getImg(), food.getPrice());
-
-    }
-}
--- back-end/cardapio/src/main/resources/db/migration/V1__create-table-foods.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE foods (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    img TEXT,
-    price INTEGER NOT NULL
-);
